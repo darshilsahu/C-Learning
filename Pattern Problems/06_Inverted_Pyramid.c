00f71f13779b61e1fafdd0beafff33726936a6bc
Darshil Sahu <darshilsahu07@gmail.com>
2026-07-24 17:48:35 +0530

    Implement Inverted Pyramid

---
+++ Pattern Problems/06_Inverted_Pyramid.c
@@ -0,0 +1,63 @@
+/*
+____________________________
+#To Print following Pattern
+     *
+    ***
+   *****
+  *******
+ ********* ....till nth lines
+ ____________________________
+*/
+#include <stdio.h>
+
+int lines;
+
+void pyramid(int n);
+void pyramid(int n){
+    for (int i = 1; i <=n; i++)
+
+    {
+        int f;
+        f=(n+1)/2;
+        if(i==1){
+            printf("%*s*",lines-f,"");
+        }
+        
+        else{
+            printf("*");
+        }
+        
+
+        
+    }
+    printf("\n");
+    if(n==1){
+        printf("%*s*",lines-1,"");
+        return;
+    }
+    
+    pyramid(n-2);
+    
+    
+    
+       
+    
+}
+   
+
+int main(){
+    //int lines;
+    printf("Enter Number of lines for pattern: ");
+    scanf("%d", &lines);
+    if (lines <= 0)
+    {
+        printf("LINES CANNOT BE ZERO OR NEGATIVE");
+    }
+    else
+    {
+        pyramid(2*lines-1);
+        
+        
+    }
+    return 0;
+}
