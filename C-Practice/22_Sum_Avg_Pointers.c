5319d682fda8bfacb19e9b71088fa1271b1e4074
Darshil Sahu <darshilsahu07@gmail.com>
2026-07-31 20:02:52 +0530

    Add Sum Average using Pointers

---
+++ C-Practice/22_Sum_Avg_Pointers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void cal(int* x,float* y);
+void cal(int* x,float* y){
+    int sum=*x+*y;
+    float avg=(*x+*y)/2.0;
+    *x=sum;
+    *y=avg;
+    
+
+    
+}
+
+int main(){
+    int a;
+    float b;
+    
+    printf("Enter First Number: ");
+    scanf("%d",&a);
+    
+    printf("Enter Second Number: ");
+    scanf("%f",&b);
+    cal(&a,&b);
+
+    
+    
+    
+    printf("The value of sum is %d\n",a);
+    printf("The value of average is %.2f\n",b);
+    return 0;
+}
