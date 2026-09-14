7b5e78e797926aa1d77398d47da05998ca8f6e31
Darshil Sahu <darshilsahu07@gmail.com>
2026-09-14 18:52:24 +0530

    Add Basic String Programs

---
+++ C-Basics/33_Strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char st[]={'H','E','L','L','O','\0'};
+    printf("%s\n",st);
+    //Second shortcut method
+    char s[]="Ohayoo";
+    printf("%s",s);
+    return 0;
+}
+
+
+
+
+
---
+++ C-Basics/34_Input_string.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    char st[10];
+    //printf("Enter Your Name: ");
+    scanf("%s", st);
+    printf("HI %s", st);
+    return 0;
+}
